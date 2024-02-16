0c39cf9f61ea72f9d27e16ad33959a08cc8ef0ad
nicolas-montani <95175152+nicolas-montani@users.noreply.github.com>
2024-02-16 19:08:51 +0100

    short version

--- pages/index.js
+++ pages/index.js
@@ -26,34 +26,8 @@ const Home = () => {
       
       <Navbar /> 
       <Hero />
-
-      <SectionTitle
-        pretitle="Testimonials"
-        title="Here's what student said">
-      </SectionTitle>
-
-      <Testimonials />
-      <SectionTitle
-        pretitle="Nextly Benefits"
-        title=" Why should you vote for us">
-        Nextly is a free landing page & marketing website template for startups
-        and indie projects. Its built with Next.js & TailwindCSS. And its
-        completely open-source.
-      </SectionTitle>
-      <Benefits data={benefitOne} />
-      <Benefits imgPos="right" data={benefitTwo} />
-      <SectionTitle
-        pretitle="Watch a video"
-        title="Learn more about us">
-        this is a short presentation video about us and our campaign.
-      </SectionTitle>
       <Video />
-      
-      <SectionTitle pretitle="FAQ" title="Frequently Asked Questions">
-        Answer your customers possible questions here, it will increase the
-        conversion rate as well as support or chat requests.
-      </SectionTitle>
-      <Faq />
+      <Benefits data={benefitOne} />
       <Cta />
       <Footer />
     </>
