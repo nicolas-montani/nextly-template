03fefd76b028833c5538cb538d5e4dbf9e4d71f9
nicolas-montani <95175152+nicolas-montani@users.noreply.github.com>
2024-02-20 12:33:31 +0100

    links update

--- components/footer.js
+++ components/footer.js
@@ -19,38 +19,52 @@ export default function Footer() {
 
 
           
-          <div className="">
-            <div>Follow us</div>
-            <div className="flex mt-5 space-x-5 text-gray-400 dark:text-gray-500">
+          <div className="flex whitespace-nowrap">
+
+            
+            <div className="m-5">Follow Lisa :
+
+            <div className="flex mt-2 mb-5 space-x-5 text-gray-400 dark:text-gray-500">
+              
               <a
-                href="https://twitter.com/web3templates"
-                target="_blank"
-                rel="noopener">
-                <span className="sr-only">Twitter</span>
-                <Twitter />
-              </a>
-              <a
-                href="https://facebook.com/web3templates"
-                target="_blank"
-                rel="noopener">
-                <span className="sr-only">Facebook</span>
-                <Facebook />
-              </a>
-              <a
-                href="https://instagram.com/web3templates"
+                href="https://www.instagram.com/lisa.militi/"
                 target="_blank"
                 rel="noopener">
                 <span className="sr-only">Instagram</span>
                 <Instagram />
               </a>
               <a
-                href="https://linkedin.com/"
+                href="https://www.linkedin.com/in/lisa-militi-584185279/"
                 target="_blank"
                 rel="noopener">
                 <span className="sr-only">Linkedin</span>
                 <Linkedin />
               </a>
+              
             </div>
+            </div>
+
+            <div className="m-5">Follow Nicolas :
+
+            <div className="flex mt-2 space-x-5 text-gray-400 dark:text-gray-500">
+            <a
+                href="https://www.instagram.com/nicolas.montani/"
+                target="_blank"
+                rel="noopener">
+                <span className="sr-only">Instagram</span>
+                <Instagram />
+              </a>
+              <a
+                href="https://www.linkedin.com/in/nicolas-montani-2bb7881b3/"
+                target="_blank"
+                rel="noopener">
+                <span className="sr-only">Linkedin</span>
+                <Linkedin />
+              </a>
+
+            </div>
+            </div>
+
           </div>
         </div>
 
@@ -60,15 +74,9 @@ export default function Footer() {
             href=""
             target="_blank"
             rel="noopener">
-            Nicolas montani.
+            Nicolas Montani.
           </a>{" "}
-          Illustrations from{" "}
-          <a
-            href="https://www.glazestock.com/"
-            target="_blank"
-            rel="noopener ">
-            Glazestock
-          </a>
+          All rights reserved.
         </div>
       </Container>
 
@@ -76,27 +84,6 @@ export default function Footer() {
   );
 }
 
-const Twitter = ({ size = 24 }) => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width={size}
-    height={size}
-    viewBox="0 0 24 24"
-    fill="currentColor">
-    <path d="M24 4.37a9.6 9.6 0 0 1-2.83.8 5.04 5.04 0 0 0 2.17-2.8c-.95.58-2 1-3.13 1.22A4.86 4.86 0 0 0 16.61 2a4.99 4.99 0 0 0-4.79 6.2A13.87 13.87 0 0 1 1.67 2.92 5.12 5.12 0 0 0 3.2 9.67a4.82 4.82 0 0 1-2.23-.64v.07c0 2.44 1.7 4.48 3.95 4.95a4.84 4.84 0 0 1-2.22.08c.63 2.01 2.45 3.47 4.6 3.51A9.72 9.72 0 0 1 0 19.74 13.68 13.68 0 0 0 7.55 22c9.06 0 14-7.7 14-14.37v-.65c.96-.71 1.79-1.6 2.45-2.61z" />
-  </svg>
-);
-
-const Facebook = ({ size = 24 }) => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width={size}
-    height={size}
-    viewBox="0 0 24 24"
-    fill="currentColor">
-    <path d="M24 12.07C24 5.41 18.63 0 12 0S0 5.4 0 12.07C0 18.1 4.39 23.1 10.13 24v-8.44H7.08v-3.49h3.04V9.41c0-3.02 1.8-4.7 4.54-4.7 1.31 0 2.68.24 2.68.24v2.97h-1.5c-1.5 0-1.96.93-1.96 1.89v2.26h3.32l-.53 3.5h-2.8V24C19.62 23.1 24 18.1 24 12.07" />
-  </svg>
-);
 const Instagram = ({ size = 24 }) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
@@ -118,6 +105,4 @@ const Linkedin = ({ size = 24 }) => (
     <path d="M22.23 0H1.77C.8 0 0 .77 0 1.72v20.56C0 23.23.8 24 1.77 24h20.46c.98 0 1.77-.77 1.77-1.72V1.72C24 .77 23.2 0 22.23 0zM7.27 20.1H3.65V9.24h3.62V20.1zM5.47 7.76h-.03c-1.22 0-2-.83-2-1.87 0-1.06.8-1.87 2.05-1.87 1.24 0 2 .8 2.02 1.87 0 1.04-.78 1.87-2.05 1.87zM20.34 20.1h-3.63v-5.8c0-1.45-.52-2.45-1.83-2.45-1 0-1.6.67-1.87 1.32-.1.23-.11.55-.11.88v6.05H9.28s.05-9.82 0-10.84h3.63v1.54a3.6 3.6 0 0 1 3.26-1.8c2.39 0 4.18 1.56 4.18 4.89v6.21z" />
   </svg>
 );
-
-
 
