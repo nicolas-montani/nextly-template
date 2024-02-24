fd738dd248c50a4a1c84ad4d649de665b3e31144
nicolas-montani <95175152+nicolas-montani@users.noreply.github.com>
2024-02-24 14:47:09 +0100

    _app.js try gtm

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,13 @@
 import { ThemeProvider } from "next-themes";
 import "../css/tailwind.css";
-
+import TagManager from 'react-gtm-module'
+import { useEffect } from 'react'
 
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    TagManager.initialize({ gtmId: 'GTM-5XZKZC7' })
+  }, [])
   return (
     <ThemeProvider attribute="class">
       <Component {...pageProps} />
